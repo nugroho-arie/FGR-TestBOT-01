79384d0eff630f30e9e41a25eed978597ae36904
nugroho-arie <arienugroho182@gmail.com>
2020-04-03 13:42:16 +0700

    clean up

--- commands/hai.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'hai',
-  description: 'perkenalan dong',
-  execute(msg, args) {
-    msg.channel.send('hai kakak namaku Mar! aku marBOT sekre fiagra yaaa. tolong jangan buang sampah sembarangan ya :((');
-  },
-};
