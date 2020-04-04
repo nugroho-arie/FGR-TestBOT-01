0f1fece8aeabc4bc5d47d9dfcef596fd3d3fc543
nugroho-arie <arienugroho182@gmail.com>
2020-04-05 06:49:58 +0700

    token Fix

--- src/config.js
+++
@@ -1,6 +0,0 @@
-{
-  "token": "Njk1NTI1Nzg3MjAxNDM3NzU3.XoeShQ.H-ko3gMCKFMsxGN2D66h_ZwvpmA",
-  "prefix": "!",
-  "ownerID": "448626469716099073"
-  //for safety => if(message.author.id !== config.ownerID) return;
-}
